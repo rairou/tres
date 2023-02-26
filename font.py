6ad676a4412e6c7c2dc62a89970ce32b560a5721
riyuzenn <riyuzenn@gmail.com>
2023-02-26 11:41:05 +0800

    rem font.py

--- font.py
+++
@@ -1,9 +0,0 @@
-
-# Rename all font files in the assets/font/ dir
-
-import pathlib
-import os
-p = "./assets/fonts/"
-for f in pathlib.Path(p).glob("**/*"):
-    x =  str(".\\" + str(f)).replace("-", "_").lower()
-    os.rename(f, x)
